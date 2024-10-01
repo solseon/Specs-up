946efa0c67f8b57896dd025130f2ea42237471d5
안수범 <solseon0130@gmail.com>
2024-10-01 16:20:34 +0900

    updata. 화정관 추가

--- 알고리즘분석및실습/NSUmap 프로젝트/main.py
+++ 알고리즘분석및실습/NSUmap 프로젝트/main.py
@@ -15,11 +15,13 @@ center = [36.908, 127.143619452225] # 남서울대학교의 [위도, 경도] 불
 m = folium.Map(location=center, zoom_start=15.5) # m 변수 지정, 남서울대학교, 화면 해상도 10
 mainbuilding = building("본관", [36.9072014, 127.1430552])
 sanggyung = building("상경학관", [36.90818157832219, 127.14407494870781])
+hw = bilsdisldislid
 
 markinglist = list() 
 
 markinglist.append(mainbuilding.marking("blue", "circle"))
 markinglist.append(sanggyung.marking("red", "circle"))
+
 
 for mark in markinglist:
     mark.add_to(m)
