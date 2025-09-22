5b27ce65c2e3eb6652ef2b92fa5dd3f5aa4f66fd
안수범 <solseon0130@gmail.com>
2025-09-22 11:28:03 +0900

    Add. C언어 반복문과 함수

---
+++ C언어/day3-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+
+    for(i=0; i < 10; i++)
+    {
+        if(i%3 == 0) // 3의 배수
+            continue;
+        printf("%d", i);
+    }
+    return 0;
+}
